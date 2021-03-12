178d504960dc53ede20239b9b1662c53760d0e47
Robert Mrn <rmrn56@gmail.com>
2021-03-12 16:53:27 +0200

    refactor: 💡 Add getters and setters for ETF model

--- apps/java-api/src/main/java/org/scoalaonline/api/model/ETF.java
+++ apps/java-api/src/main/java/org/scoalaonline/api/model/ETF.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class ETF
 {
 
-  //region Fields
+  //--------------- Fields ---------------
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "etf_id")
@@ -35,4 +35,62 @@ public class ETF
 
   public ETF(){
   }
+  //--------------- Getters ---------------
+  public long getId(){
+    return id;
+  }
+
+  public String getEtfName(){
+    return name;
+  }
+
+  public String getEtfTicker(){
+    return tricker;
+  }
+
+  public Map<String, Double> getStock1(){
+    return stock_1;
+  }
+
+  public Map<String, Double> getStock2(){
+    return stock_2;
+  }
+
+  public Map<String, Double> getStock_3(){
+    return stock_3;
+  }
+
+  public Map<String, Double> getStock_4(){
+    return stock_4;
+  }
+
+  //--------------- Setters ---------------
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public void setEtfName (String name){
+    this.name = name;
+  }
+  public void setEtfTicker (String ticker){
+    this.ticker = ticker;
+  }
+
+  public void setEtfStock1 (String stock_ticker, Double stock_value){
+    this.stock_1 = new HashMap<String, Double>(stock_ticker, stock_value);
+  }
+
+  public void setEtfStock1 (String stock_ticker, Double stock_value){
+    this.stock_2 = new HashMap<String, Double>(stock_ticker, stock_value);
+  }
+
+  public void setEtfStock1 (String stock_ticker, Double stock_value){
+    this.stock_3 = new HashMap<String, Double>(stock_ticker, stock_value);
+  }
+
+  public void setEtfStock1 (String stock_ticker, Double stock_value){
+    this.stock_4 = new HashMap<String, Double>(stock_ticker, stock_value);
+  }
+
+
 }
