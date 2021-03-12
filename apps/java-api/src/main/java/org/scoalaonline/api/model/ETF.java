c348dd0669b570273153da27a0d08cdf0a897f75
Robert Mrn <rmrn56@gmail.com>
2021-03-12 16:10:17 +0200

    refactor: 💡 Add atributes and constructor to class ETF
    I added the id, name and hasmaps necessary as the atributes of ETF and
    an empty constructor.

--- apps/java-api/src/main/java/org/scoalaonline/api/model/ETF.java
+++ apps/java-api/src/main/java/org/scoalaonline/api/model/ETF.java
@@ -1,4 +1,37 @@
 package org.scoalaonline.api.model;
 
-public class ETFs {
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "etf")
+public class ETF
+{
+
+  //region Fields
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "etf_id")
+  private long id;
+
+  @Column(name = "etf_name", nullable = false, length = 50)
+  private String name;
+
+  @Column(name = "etf_ticker", nullable = false, length = 10)
+  private String ticker;
+
+  @Column(name = "stock_1", nullable = false)
+  private Map<String, Double> stock_1;
+
+  @Column(name = "stock_2", nullable = false)
+  private Map<String, Double> stock_2;
+
+  @Column(name = "stock_3", nullable = true)
+  private Map<String, Double> stock_3;
+
+  @Column(name = "stock_4", nullable = true)
+  private Map<String, Double> stock_4;
+
+  public ETF(){
+  }
 }
