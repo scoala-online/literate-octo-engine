8d3428b95b5314024c2ecba9de40d3007c04610d
Sergiu <sergiulius_147@yahoo.com>
2021-03-12 15:40:03 +0200

    refactor: 💡 Add for Robert to work

--- apps/java-api/src/main/java/org/scoalaonline/api/model/Stock.java
+++ apps/java-api/src/main/java/org/scoalaonline/api/model/Stock.java
@@ -5,5 +5,20 @@ import java.util.Objects;
 
 @Entity
 @Table(name="stocks")
-public class Stocks {
+public class Stocks
+{
+  //region Fields
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "stock_id")
+  private long id;
+
+  @Column(name = "stock_name", nullable = false, length = 20)
+  private String stockName;
+
+  @Column(name = "stock_ticker", nullable = false, length = 10)
+  private String tickerName;
+
+  public Stock() {
+  }
 }
