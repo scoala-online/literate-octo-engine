f83b5b6d9adbf12ebd23096cc56af9042dfd0f21
Robert Mrn <rmrn56@gmail.com>
2021-04-07 21:44:17 +0300

    feat: 🎸 Add repo for ETF and Stock. Modify ETFStock repo

--- apps/java-api/src/main/java/org/scoalaonline/api/repo/ETFStockRepo.java
+++ apps/java-api/src/main/java/org/scoalaonline/api/repo/ETFStockRepo.java
@@ -3,10 +3,12 @@ package org.scoalaonline.api.repo;
 import org.scoalaonline.api.model.ETF;
 import org.scoalaonline.api.model.ETFStock;
 import org.scoalaonline.api.model.ETFStockKey;
+import org.scoalaonline.api.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ETFStockRepo extends JpaRepository<ETFStock, ETFStockKey> {
   List<ETFStock> findETFStocksByEtf(ETF etf);
+  List<ETFStock> findETFStocksByStock(Stock stock);
 }
